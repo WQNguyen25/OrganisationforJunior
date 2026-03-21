cf5c8bc15a17709f2daa2460d0e4695bc65ecf69
WQNguyen25 <wnguyen297@gmail.com>
2026-03-21 14:29:19 +0000

    adding display functionality for checkbox

--- scripts.js
+++ scripts.js
@@ -1,13 +1,23 @@
-function myFunction() {
-  // Get the value of the input field with id="numb"
-  let x = document.getElementById("numb").value;
-  // determine whether the value is 1 or 2
-  let text;
-  if (x === "1") {
+
+document.getElementById("timeForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // stop page reload
+
+    const selectedTimes = [];
+
+    document.querySelectorAll('input[name="time"]:checked').forEach((checkbox) => {
+        selectedTimes.push(checkbox.value);
+    });
+
+    console.log("Selected times:", selectedTimes);
     
-  } else if (x === "2") {
-    
-  } else {
-    text = "invalid input";
-  }
-}
+
+        // Display values
+    const outputDiv = document.getElementById("output");
+
+    if (selectedTimes.length > 0) {
+        outputDiv.innerHTML = "You selected: " + selectedTimes.join(", ") + "am";
+    } else {
+        outputDiv.innerHTML = "No time selected";
+    }
+});
+
